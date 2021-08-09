ee53d9a413b2fec582dab4acac18fac85c35bda3
derek <derekle.creative@gmail.com>
2021-08-09 05:39:56 -0500

    commit tree 7ebe722e0e81f23948c8955e0519dd8f3cb891df

--- index.js
+++ index.js
@@ -10,4 +10,27 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(x){
+  let t = parseInt(x.split(":"));
+  console.log(x)
+  console.log(t)
+  console.log(t < 12)
+  console.log(12 < t && t < 17)
+  console.log(17 < t)
+  if (t < 12) {
+    
+    return "Good Morning";
+  } else if (12 < t && t < 17) {
+    
+    return "Good Afternoon";
+  } else {
+    
+    return 'Good Evening';
+  }
+}
 /* Write your implementation of displayMessage() */
+function displayMessage(x) {
+  console.log(x)
+  let element = document.getElementById('greeting');
+  element.innerHTML = x
+}
